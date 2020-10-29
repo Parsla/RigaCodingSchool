124d15d01481eacd4e191f2584b3909c13303e28
Parsla <parsla.eberlina@gmail.com>
2020-10-29 19:16:31 +0200

    Added Day5

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -4,9 +4,43 @@ namespace Day4
 {
     class Program
     {
+        static void Task1()
+        {
+            string[] array = { "Jānis", "Pēteris", "Kārlis" };
+            int x = 0;
+
+            while (x < 3)
+                {
+                
+            }
+        }
+        static void Task2()
+        {
+            int[] numbers = { 1, 3, 4, 8, 0, 23 };
+            string str = string.Join(" , ", numbers);
+            Console.WriteLine(str);
+            for (int i = 0; i > 5  ; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+        }
+        static void Task3()
+        {
+            string[] array = { "Jānis", "Pēteris", "Kārlis" };
+        }
+        static void Task4()
+        {
+
+        }
+        static void Task5()
+        {
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Task1();
+            Task2();
         }
     }
 }
